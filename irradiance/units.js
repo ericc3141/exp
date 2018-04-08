f664d6df720a9624c75d84c701a694e7acc01014
eric <ericc3141@users.noreply.github.com>
2018-04-08 10:33:31 -0700

    whoops

---
+++ irradiance/units.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var IRRADIANCE = 1364; // Watts per square meter
+var POWER = { // in watts
+    "Average Flashlight":1,
+    "People":100.,
+    "Intel Atoms":2.5,
+    "AMD Threadrippers":324.4,
+    "Toasters":1200,
+    "Horsepower":745.7,
+    "\"Prii\"":121*745.7,
+    "Dodge Demons":840*745.7,
+    "Lightsabers":6.96e6,
+    "Large Hadron Colliders":200e6,
+    "Nuclear Power Plants":3937e6,
+};
+var AREA = { // in sq meters
+    "60\" TV":0.992,
+    "16\" Pizza":0.1297,
+    "Suntan":0.6,
+    "Avery Single":11.52,
+    "Tennis Court":260.87,
+    "City block":9290.304,
+    "California":423.97e9,
+}
